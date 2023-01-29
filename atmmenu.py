8018ad726f3fe284cef672f036be03f690a025ed
kkirankumar123 <kkirankumar2396@gmail.com>
2023-01-29 14:08:38 +0530

    atm app

--- atmmenu.py
+++ atmmenu.py
@@ -6,6 +6,6 @@ def atmmenu():
     print("===================================")
     print("\t1.DEPOSIT")
     print("\t2.WITHDRAW")
-    print("\t3.BALANC ENQ")
+    print("\t3.BALANCE ENQUIRY")
     print("\t4.EXIT")
     print("===================================")
